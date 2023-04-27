5f57aa029d2c30f85b1dd50c8aeab7ea80d9ee14
Dmitry Shishkov <99988422+DimDimShishkov@users.noreply.github.com>
2023-04-28 01:15:40 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+  <SideBar />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import SideBar from "./components/SideBar.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    SideBar,
   },
 });
 </script>
 
 <style>
+@import "./assets/styles/normalize.css";
+@import "./assets/styles/global.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
 }
+
+/* .v-collapse {
+  transition: height var(--vc-auto-duration) cubic-bezier(0.33, 1, 0.68, 1);
+} */
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { Collapse } from "./hooks";
 
-createApp(App).mount("#app");
+createApp(App).component("Collapse", Collapse).mount("#app");
